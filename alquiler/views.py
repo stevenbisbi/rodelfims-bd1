2ab04aeeb0cdb9831d91679541f7518107b50326
Michael <stevenex13@gmail.com>
2024-10-13 21:35:33 -0500

    add signin

--- alquiler/views.py
+++ alquiler/views.py
@@ -22,7 +22,19 @@ def alquiler(request):
     return render(request, 'alquiler.html',)
 
 def signin(request):
-    return render(request, 'signin.html')
+    if request.method == 'GET':
+        return render(request, 'signin.html', {'form': AuthenticationForm} )
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm, 'error': 'Nombre de usuario o contraseña incorrectos'
+            })
+        else:
+            login(request, user)
+            return redirect('home')
 
 def signup(request):
     if request.method == 'GET':
